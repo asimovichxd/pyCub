514dd897449298cb1e68d4a20f07d47eb3727383
asimovichxd <carlodasamo@gmail.com>
2025-02-24 15:19:54 +0100

    Update push_the_ball_pure_joints.py
    Fixed file import

--- icub_pybullet/examples/push_the_ball_pure_joints.py
+++ icub_pybullet/examples/push_the_ball_pure_joints.py
@@ -11,7 +11,7 @@ except:
     import sys
     import os
     sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-    from pycub import pyCub
+    from icub_pybullet.pycub import pyCub
 
 
 def push_the_ball(client):
